d65dabc243f9600a7634b959e79946536a08b458
POOJA <95736907+poojameh9266@users.noreply.github.com>
2022-09-19 18:53:34 -0600

    Update streamlit_app.py
    Added another menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.text('🥣 Omeaga 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach, Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free-Range Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
