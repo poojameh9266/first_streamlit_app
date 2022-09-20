8f7179be8d44a8727b04ed9754c0ea2cb4ff4b9e
POOJA <95736907+poojameh9266@users.noreply.github.com>
2022-09-19 18:52:39 -0600

    Update streamlit_app.py
    Added emoji;s

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omeaga 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach, Rocket Smoothie')
-streamlit.text('Hard Boiled Free-Range Eggs')
+streamlit.text('🥣 Omeaga 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach, Rocket Smoothie')
+streamlit.text('🐔 Hard Boiled Free-Range Eggs')
+streamlit.text('🥑🍞 Avocado Toast')
