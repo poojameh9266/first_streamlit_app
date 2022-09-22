c9038beb8224caee870ec22c0fcdd3c52fe1d9ce
POOJA <95736907+poojameh9266@users.noreply.github.com>
2022-09-22 05:10:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 #Display only the selected values
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-#streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
