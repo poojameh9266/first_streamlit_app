39dfefe4baf4036f317336f85522319f8e93365a
POOJA <95736907+poojameh9266@users.noreply.github.com>
2022-10-14 14:50:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,18 +32,34 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+#old way
 #New function to display fruitvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# take the json version and  Normalize the date
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  #streamlit.header("Fruityvice Fruit Advice!")
+  #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  #streamlit.write('The user entered ', fruit_choice)
+  #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  
+  # take the json version and  Normalize the date
+    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output in a table format using dataframe.
-streamlit.dataframe(fruityvice_normalized)
+    #streamlit.dataframe(fruityvice_normalized)
+
+#Above old way in new organized way.  
+#New function to display fruitvice api response
+  streamlit.header("Fruityvice Fruit Advice!") 
+  try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+      streamlit.error ("Please select a fruit to get information.")
+    else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+      
+ except URLError as e:
+  streamlit.error()
+
+
 
 #put the stop so stremlit does not run anything after this statement
 streamlit.stop()
