45193c2408b73a53215e5b000773e1d1cb4c0ddb
POOJA <95736907+poojameh9266@users.noreply.github.com>
2022-10-14 15:38:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,21 +79,35 @@ if streamlit.button('Get fruit load list'):
   streamlit.dataframe(my_data_rows)
   
 
+#Allow end user to add new fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
+    return "Thanks for adding" + new_fruit
+  
+add_my_fruit = streamlit.text_input('What fruit would you like to add')  
+if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+  
+  
 #put the stop so stremlit does not run anything after this statement
 streamlit.stop()
 
 
 
 
+#old way
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit_load_list contains:")
+#streamlit.dataframe(my_data_rows)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit_load_list contains:")
-streamlit.dataframe(my_data_rows)
+#old way
+#add_my_fruit = streamlit.text_input('What fruit would you like to add')
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add')
-
-streamlit.write('Thanks for adding', add_my_fruit)
-my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
+#streamlit.write('Thanks for adding', add_my_fruit)
+#my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
